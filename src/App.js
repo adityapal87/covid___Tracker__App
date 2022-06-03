cdf5b9ea324872e1e4af269bf8aa72689e47ac85
Aditya Kumar <100465366+adityapal87@users.noreply.github.com>
2022-06-03 11:11:04 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import styles from './App.module.css';
 import { fetchData } from './components/api/index';
-// import { Cards , Chart , CountryPicker } from './components';
+
 import Cards from './components/Cards/cards';
+import  Chart  from './components/Chart/chart';
+import  CountryPicker  from './components/CountryPicker/countryPicker';
+// import { Cards , Chart , CountryPicker } from './components';
 
 class App extends React.Component{
 
- async componentDidMount(){
-    const data = await fetchData();
-    console.log(data);
-  }
+  state={
+    data:{},
+  };
  
+ async componentDidMount(){
+    const fetchedData = await fetchData(); // fetching data from api
+    // console.log(fetchedData); // to check what data inside this { fetchedData }
+    this.setState({ data : {fetchedData} }) //updating data's value's
+  }
+//  data.fetchedData.confirmed
   render(){
+    const { data } = this.state;
     return(
-      <div className={styles.container}>
-        <Cards />
-        {/* <Chart /> */}
-        {/* <CountryPicker /> */}
+      <div className = {styles.container}>
+        <Cards data = {data.fetchedData}/>
+        <CountryPicker />
+        <Chart />
       </div>
     );
   }
